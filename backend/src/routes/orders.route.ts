707f07bc49906952e3e4abec89451de167f3ba77
nickchengunsw <z5208270@unsw.edu.au>
2022-06-21 15:07:41 +1000

    add api

--- backend/src/routes/orders.route.ts
+++ backend/src/routes/orders.route.ts
@@ -1,30 +1,18 @@
 import { Response } from 'express';
 import { OrdersObject } from '../models/orderObject.model';
 import { RequestBody } from '../types/ordersPostRequest';
-import axios from 'axios';
+import { request } from '../controller/controller'
 
 async function createOrder (req: RequestBody<OrdersObject>, res: Response){
-    const order = {
-        ...req.body,
-        product: 'pay-in-3'
-    }
-    const options = {
-        method: 'POST',
-        url: 'https://integration.api.scalapay.com/v2/orders',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          Authorization: `Bearer qhtfs87hjnc12kkos`
-        },
-        data: order
-    };
-    axios.request(options).then(response => {
+    try {
+        const orderObject = req.body;
+        const response = await request(orderObject)
         res.status(200).json(response.data);
-      }).catch(error => {
+    } catch (error : any) {
         return res.status(error.response.status).json([
             {error: error.response.data},
         ])
-      });
+    }
 }
 
 export default createOrder;
